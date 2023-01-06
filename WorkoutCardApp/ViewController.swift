b288371f3acf32a816bbf2b685fcfa086c4abb29
msnbek <mahmutsenbek@icloud.com>
2023-01-06 13:51:47 +0300

    Initial Commit

---
+++ WorkoutCardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutCardApp
+//
+//  Created by Mahmut Senbek on 6.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
